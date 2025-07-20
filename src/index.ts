bdea178770d00e3f3e7b92853cfc5319ee090acd
Harshcodesfast <harshawalegaonkar@gmail.com>
2025-07-20 11:52:46 +0530

    temp3

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,22 @@ import express from "express";
 import serverless from "serverless-http";
 import dotenv from "dotenv";
 
-dotenv.config({ path: ".env" });
+// dotenv.config({ path: ".env" });
 
 const app = express();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-if (process.env.ENV == "local") {
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-}
+// if (process.env.ENV == "local") {
+//   app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+//   });
+// }
 
 app.get("/", (req, res) => {
   res.json({ message: "service is running" });
+});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
